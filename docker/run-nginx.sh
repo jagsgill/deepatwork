43c5727b4e53b46d3b76847e352c72a8b3010608
Jagdeep Gill <jagdeep.gill232@gmail.com>
2018-08-05 06:28:23 +0000

    Add restart policy for nginx container

--- docker/run-nginx.sh
+++ docker/run-nginx.sh
@@ -2,4 +2,4 @@
 
 cd ../
 hugo
-exec docker run -d -p 80:80 -v $PWD/public:/usr/share/nginx/html:ro nginx
+exec docker run --restart unless-stopped -d -p 80:80 -v $PWD/public:/usr/share/nginx/html:ro nginx
